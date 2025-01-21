febe48ee3281b5c05c906d6241921f339be2a639
sandhyaa7 <basandhya@yahoo.com>
2025-01-21 15:21:52 -0600

    More code added to index.js file

--- index.js
+++ index.js
@@ -44,3 +44,112 @@ function calculateArea(width, height) {
    return width * height;
 }
 console.log(calculateArea(10, 20));
+
+//Operators
+//Arithmetic operators
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+//Increment(++)
+console.log(++num1);
+console.log(num1++);
+console.log(num1);
+
+//Decrement(--)
+console.log(--num1);
+
+
+//Assignment Operators
+let num3 = 10;
+num3 += 20;
+console.log(num3);
+num3 -= 20;
+console.log(num3);
+num3 *= 20;
+console.log(num3);
+
+//Comparison Operators
+let num4 = 10;
+let num5 = '10';
+console.log(num4 == num5);//loose equality operator
+console.log(num4 != num5);
+console.log(num4 > num5);
+console.log(num4 < num5);
+console.log(num4 >= num5);
+console.log(num4 <= num5);
+console.log(num4 === num5);//Strict equality operator
+console.log(num4 !== num5);
+
+//Ternary Operator
+let points = 1000;
+let type = points > 100 ? 'gold' : 'silver';
+console.log(type);
+
+//Logical Operators
+//logical And(&&) returns true if both operands are TRUE
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a && true);
+
+//logical OR(||) returns true if one of the operands is TRUE
+console.log(a || b);
+
+//NOT(!)
+let isAnny = false;
+console.log(!isAnny);
+
+//Logical operators with non-booleans
+//Falsy
+//Undefined
+//Null
+//0
+//false
+//NaN
+//Empty string ('')
+//Anything that is not falsy is Truthy
+
+let userColor = undefined;
+let defaultColor = 'blue';
+let currentColor = userColor || defaultColor;
+console.log(currentColor);
+
+
+
+//Operator precedence
+//1. Parentheses
+//2. Exponentiation
+//3. Multiplication and Division
+//4. Addition and Subtraction
+
+let x = (2 + 3) * (4 - 1);
+console.log(x);
+
+let y = 10;
+let z = (y > 10) ? 1 : 0;
+console.log(z);
+
+let as = (1 == true);
+console.log(as);
+
+let r = 10;
+let t = r++;
+console.log(t);
+console.log(r++);
+
+
+//Exercise
+let a1 = 'red';
+let b1 = 'blue';
+let c1 = a1;
+a1 = b1;
+b1 = c1;
+console.log(a1);
+console.log(b1);
+
